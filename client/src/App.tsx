f3fed028c7a59d021185e9fe09cffa5777f678d9
jhyun860-source <jhyun860@naver.com>
2026-06-01 14:58:40 +0000

    [fix] /settlement 라우트 App.tsx에 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,7 @@ import Login from "./pages/Login";
 import TableReport from "./pages/TableReport";
 import StaffIncentiveStats from "./pages/StaffIncentiveStats";
 import LiquorStockReport from "./pages/LiquorStockReport";
+import SettlementDashboard from "./pages/SettlementDashboard";
 import { UpdateBanner } from "./components/UpdateBanner";
 function Router() {
   // make sure to consider if you need authentication for certain routes
@@ -25,6 +26,7 @@ function Router() {
       <Route path={"/table-report"} component={TableReport} />
       <Route path={"/staff-incentive"} component={StaffIncentiveStats} />
       <Route path={"/liquor-stock"} component={LiquorStockReport} />
+      <Route path={"/settlement"} component={SettlementDashboard} />
       <Route path={"/404"} component={NotFound} />
       <Route component={NotFound} />
     </Switch>
